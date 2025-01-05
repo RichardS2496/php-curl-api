452881e50f7c231a77057b7f9648396facc1b984
Richard Suarez <richardmsc24@gmail.com>
2025-01-05 22:45:32 +0100

    titles fixed

--- next-release.php
+++ next-release.php
@@ -23,7 +23,7 @@ curl_close($ch);
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?php echo $title; ?></title>
+        <title><?php echo $data["following_production"]["title"]."|".$title; ?></title>
         <link rel="stylesheet" href="./styles/styles.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
