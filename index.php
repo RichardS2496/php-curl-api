f7c148269b4faf1fa15e7bb295e7a95fa263c3f5
Richard Suarez <richardmsc24@gmail.com>
2025-01-05 01:57:59 +0100

    uploading main structure

--- index.php
+++ index.php
@@ -12,12 +12,12 @@ $result= curl_exec($ch);
 if ($result === false){
     echo "Error cURL". curl_error($ch);
 } else {
-    var_dump($result);
     $data=json_decode($result, true);
-    var_dump($data);
 }
 curl_close($ch);
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang= "en">
@@ -30,6 +30,35 @@ curl_close($ch);
     
   
     <body>
-        
+        <div class="main-container">
+
+            <header>
+                <img src="./assets/LOGO.png" alt="Next MCU Releases logo">
+            </header>
+            <main>
+                <div class="overlay"></div>
+                <section class="film-info">
+                    <div class="movie-description">
+                        <span><?=$data["type"]?></span>
+                        <h1><?= $data["title"]?></h1>
+                        <p><?= $data["overview"]?></p>
+                        <div>
+                            <p>Release date</p>
+                            <?php
+                                $dateByParts = explode('-', $data["release_date"]);
+                                $formattedDate = $dateByParts[2].'-'.$dateByParts[1].'-'.$dateByParts[0];
+                            ?>
+                            <h2><?=$formattedDate?></h2>
+                        </div>
+                    </div>
+                    <div class="img-container">                    
+                        <img src="<?= $data["poster_url"] ?>" alt="Poster de <?=$data["title"] ?>">
+                    </div>
+                </section>
+            </main>
+            <footer>
+                <p>FOOTER SECTION</p>
+            </footer>
+        </div>
     </body>
 </html>
